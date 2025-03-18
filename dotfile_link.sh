f880abd9c31747b0738177d3c46e4093795002d0
sniper-fly <ryo.nakai0@gmail.com>
2025-03-18 11:46:04 +0900

    dpx関数をgithubからダウンロードして更新するように移動し、.gitignoreにdpxを追加

--- dotfile_link.sh
+++ dotfile_link.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ln -sf "`pwd`"/vimrc ~/.vimrc
-ln -sf "`pwd`"/zshrc ~/.zshrc
+ln -sf "$(pwd)"/vimrc ~/.vimrc
+ln -sf "$(pwd)"/zshrc ~/.zshrc
+./modules/sync_dpx.sh
---
+++ modules/sync_dpx.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -o ./dpx https://raw.githubusercontent.com/sniper-fly/node_docker_images/main/dpx
